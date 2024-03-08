828bef657f425ed95034e422d6ea552d355adaa3
pc 01 do lab <robota.das.ufsc@gmail.com>
2024-03-08 20:25:16 -0300

    sla n deu mto certo

--- main.cpp
+++ main.cpp
@@ -1,48 +1,48 @@
 #include <cstdio>
-#include <Matrix.hpp>
+#include "matriz_solver\Matrix.hpp"
 using namespace std;
 
-// quantidade de vezes q repete o experimento
-int qtd_testes = 5, inicio = 0, final = 10, degraus = 10;
+#define pi 3.1415926
 
-// matriz para analise de dados
-// col: index_teste, raio, x0, y0, n_pontos, radianos, tempo, ruido, metodo // 9 elementos
-Matrix all(qtd_testes*degraus, 9);
+// quantidade de vezes q repete o experimento
+double inicio = 0, final = 5, degraus = 5;
 
 int main() {
     // variaveis de cada experimento
-    int inicio_pontos = 3, final_pontos = 15, degraus_pontos = 15;  
-    float inicio_radianos = 3.1415926/4, final_radianos = 2*3.1415926, degraus_radianos = 7;  
-    int inicio_raio = 1, final_raio = 10, degraus_raio = 9;  
-    int inicio_xy0 = 0, final_xy0 = 10, degraus_xy0 = 10;  
+    /*double inicio_pontos    = 03,   final_pontos    = 31,   degraus_pontos      = 14;  
+    double inicio_radianos  = pi/4, final_radianos  = 2*pi, degraus_radianos    = 07;  
+    double inicio_raio      = 01,   final_raio      = 20,   degraus_raio        = 10;  
+    double inicio_xy0       = 00,   final_xy0       = 10,   degraus_xy0         = 10;  
+    double inicio_ruido     = 00,   final_ruido     = 10,   degraus_ruido      = 10;
 
-    for(int index_teste = inicio; index_teste < final; index_teste += (final-inicio/degraus)){
-        for(int i = 0; i < qtd_testes; i++){        // repete o mesmo experimento para redundancias
-            // dados de cada experimento
-            int media, max, min, desv_padrao; 
-            // dados do circulo
-            int raio, x0, y0;
+    // col: teste, raio, x0, y0, n_pontos, radianos, tempo, ruido, metodo // 9 elementos
+    double linhas = (degraus+1)*(degraus_pontos+1)*(degraus_radianos+1)*(degraus_raio+1)*(degraus_xy0+1);
+    Matrix all(linhas, 9);
 
-            // desloca o centro
-            for(int desloc_xy = inicio_xy0; desloc_xy < final_xy0; desloc_xy += (inicio_xy0-final_xy0/degraus_xy0)){
-                // aumenta o raio
-                for(int r = inicio_raio; r < final_raio; r+= (final_raio-inicio_raio/degraus_raio)){
-                    // aumenta a circunferencia
-                    for(float rad = inicio_radianos; rad < final_radianos; rad += (final_radianos-inicio_radianos/degraus_radianos)){
-                        // aumenta a quantidade de pontos
-                        for(){
-                            
-                        }
+    // repete o mesmo experimento para redundancias
+    double contador = 0;
+    for(double teste = inicio; teste <= final; teste += ((final-inicio)/(degraus))){   
+        // dados de cada experimento
+        int media, max, min, desv_padrao; 
+        // dados do circulo
+        int raio, x0, y0;
+
+        // desloca o centro
+        for(double desloc_xy = inicio_xy0; desloc_xy <= final_xy0; desloc_xy += ((final_xy0-inicio_xy0)/(degraus_xy0))){
+            // aumenta o raio
+            for(double r = inicio_raio; r <= final_raio; r += ((final_raio-inicio_raio)/(degraus_raio))){
+                // aumenta a circunferencia
+                for(double rad = inicio_radianos; rad <= final_radianos; rad += ((final_radianos-inicio_radianos)/(degraus_radianos))){
+                    // aumenta a quantidade de pontos
+                    for(double n_pontos = inicio_pontos; n_pontos <= final_pontos; n_pontos += ((final_pontos-inicio_pontos)/(degraus_pontos))){
+                        // debug printf("%.2f/%.2f\tteste: %.2f\txy0: %.2f\traio: %.2f\trad: %.2f\tn pontos: %.2f\n", ++contador, linhas, teste, desloc_xy, r, rad, n_pontos);
+                    
+
                     }
                 }
             }
-            Matrix pontos(n_pontos, 2);
-
-
         }
-    }
-    
-
+    }*/
 
     printf("aqui\n");
 
